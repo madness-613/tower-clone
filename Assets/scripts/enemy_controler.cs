d56e2d3c94937eb96f296f2f0bf40a997f3c3b1e
madness-613 <austinfaulknerjanes2008@gmail.com>
2023-01-13 11:07:46 -0500

    enemy move ai

--- Assets/scripts/enemy_controler.cs
+++ Assets/scripts/enemy_controler.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class enemy_controler : MonoBehaviour
 {
+  private GameObject enemy;
+  private GameObject player;
+  private enemy_stats stats;
     // Start is called before the first frame update
     void Start()
     {
-        
+      enemy = this.gameObject;
+      player = GameObject.Find("tower");
+      stats = this.gameObject.GetComponent<enemy_stats>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+      var step =  stats.moveSpeed * Time.deltaTime;
+      enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, player.transform.position, step);
     }
 }
